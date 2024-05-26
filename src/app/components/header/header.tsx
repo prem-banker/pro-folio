97679494f35dac7335276a2f236e055b31be676c
Prem Banker <bankerprem3@gmail.com>
2024-05-26 01:56:03 -0400

    committing header as  completed for now

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,54 +1,45 @@
 // components/Header.tsx
 import React from 'react';
 import Link from 'next/link';
+import NavItem from './navitem';
 
 const Header: React.FC = () => {
   return (
     <header className="bg-primaryLightNavyBlue text-secondaryLightBlue h-16 flex items-center justify-between border-b border-line rounded-lg">
       <div className="container mx-auto  h-full ">
-        <div className="flex space-x-4  h-full items-center">
+        <div className="flex   h-full items-center">
           <div className="w-[311px] pl-[22px] border-r border-line h-full flex-vertical-center">
-            <Link href="/" passHref className="  hover:text-secondaryFluorescentGreen">
+            <Link href="/" passHref className="hover:text-secondaryFluorescentGreen">
               prem-banker
             </Link>
           </div>
 
-          <Link
+          <NavItem href="/" text="_hello" paddingX={18} />
+
+          <NavItem href="/about" text="_about-me" paddingX={18} />
+
+          <NavItem href="/projects" text="_projects" paddingX={18} />
+
+          {/* <Link
             href="/"
             passHref
             className="text-labels border border-secondaryWhite p-2 hover:bg-secondaryLightBlue">
 
             Home
 
-          </Link>
-          <Link
+          </Link> */}
+          {/* <Link
             href="/about"
             passHref
             className="text-labels border border-secondaryWhite p-2 hover:bg-secondaryLightBlue">
 
             About Me
 
-          </Link>
-          <Link
-            href="/projects"
-            passHref
-            className="text-labels border border-secondaryWhite p-2 hover:bg-secondaryLightBlue">
+          </Link> */}
 
-            Projects
-
-          </Link>
         </div>
       </div>
-      <div>
-        <Link
-          href="/contact"
-          passHref
-          className="text-labels border border-secondaryWhite p-2 hover:bg-secondaryLightBlue">
-
-          Contact Me
-
-        </Link>
-      </div>
+      <NavItem href="/contact" text="_contact" paddingX={18} borderLeft={true} borderRight={false}  />
     </header>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Fira_Code, Inter } from "next/font/google";
 
 import "./globals.css";
-import Header from "./components/header";
+import Header from "./components/header/header";
 import Footer from "./components/footer";
 
 // const inter = Inter({ subsets: ["latin"] });
