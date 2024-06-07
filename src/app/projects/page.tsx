572da992af7d71e7de6f409860f04bfd274901a0
Prem Banker <bankerprem3@gmail.com>
2024-06-07 18:54:43 -0400

    changes to active tech sidebar

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,32 @@
-import Link from "next/link";
-import { FaArrowDown, FaCaretDown, FaChevronDown } from "react-icons/fa";
+"use client";
+
+import { useState } from "react";
+import { FaCaretDown } from "react-icons/fa";
 import meta from "../../../public/data/meta.json";
-import { getIconComponent, standardizeName } from "../utils/utils";
 import TechIcon from "../components/techicon";
 import "../styles/projects.css";
+import { standardizeName } from "../utils/utils";
 
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
   const { technologies } = meta;
+  const [activeTechnologies, setActiveTechnologies] = useState(technologies);
+
+  const toggleCheckbox = (tech: string) => {
+    if (activeTechnologies.includes(tech)) {
+      setActiveTechnologies(activeTechnologies.filter((t) => t !== tech));
+    } else {
+      setActiveTechnologies([...activeTechnologies, tech]);
+    }
+  };
+
   return (
     <div className="flex flex-col h-full">
       {/* Header */}
       <div className="h-[40px] flex items-center justify-between border-b border-line">
         <div className="flex-vertical-center">
           <div className="w-[311px] pl-[22px] border-r border-line  flex-vertical-center">
-            <FaCaretDown className="text-white  mr-2" />
+            <FaCaretDown className="text-white mr-2" />
             <span className="text-white">projects</span>
           </div>
         </div>
@@ -27,10 +39,18 @@ export default function Page() {
         <div className="w-[311px] border-r border-line h-full">
           {technologies.map((tech, index) => (
             <div key={index} className="flex items-center mt-4 mx-[22px]">
-              <input type="checkbox" id={tech} className="h-4 w-4" />
+              <input
+                type="checkbox"
+                id={tech}
+                className="h-4 w-4"
+                checked={activeTechnologies.includes(tech)}
+                onChange={() => toggleCheckbox(tech)}
+              />
               <label
                 htmlFor={tech}
-                className="flex items-center space-x-2 ml-4"
+                className={`flex items-center space-x-2 ml-4 ${
+                  activeTechnologies.includes(tech) ? "text-white" : ""
+                }`}
               >
                 <TechIcon tech={tech} />
                 <span>{standardizeName(tech)}</span>
