721cb9222eea2d536a6e9ed2f8373900ee9e0fc7
Prem Banker <bankerprem3@gmail.com>
2024-08-07 18:28:18 -0400

    sorted bio page

--- src/app/about/bio/page.tsx
+++ src/app/about/bio/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import React from "react";
 import userdata from "../../../../public/data/user.json";
+import CustomCodeEditor from "@/app/components/code-editor/codeeditor";
+import { addLineBreaks } from "@/app/utils/utils";
 
 export default function BioPage({}: {}) {
-  return <div>{userdata.user.bio}</div>;
+  return (
+    <div className="h-full">
+      <CustomCodeEditor code={addLineBreaks(userdata.user.bio, 50)} />
+    </div>
+  );
 }
